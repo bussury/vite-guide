a01c14d2661f14bdc09bdde74464753f0b58fb03
bussury <bussury.omary@gmail.com>
2022-03-31 11:22:52 +0300

    vite upgrade to v2.9

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-/**
- * @type {import('vite').UserConfig}
- */
-export default {
-  plugins: [vue()],
-}
+
+// /**
+//  * @type {import('vite').UserConfig}
+//  */
+// export default {
+//   plugins: [vue()],
+// }
+export default defineConfig({
+  plugins:[ vue() ]
+})
